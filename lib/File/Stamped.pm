f9f465db9ae87655a0c39ae0364d8b09d07905d5
tokuhirom <tokuhirom@gmail.com>
2013-01-20 18:57:32 +0900

    You can specify the file generation rule as coderef.

--- lib/File/Stamped.pm
+++ lib/File/Stamped.pm
@@ -10,7 +10,13 @@ use SelectSaver ();
 sub new {
     my $class = shift;
     my %args = @_==1?%{$_[0]}:@_;
-    $args{pattern} || Carp::croak "missing mandatory parameter 'pattern'";
+    if (exists($args{pattern}) && exists($args{callback})) {
+        Carp::croak "Both 'pattern' and 'callback' cannot be specified.";
+    }
+    unless (exists($args{pattern}) || exists($args{callback})) {
+        Carp::croak "You need to specify 'pattern' or 'callback'.";
+    }
+    my $callback = delete($args{callback}) || _make_callback_from_pattern(delete($args{pattern}));
     my $self = bless \do { local *FH }, $class;
     tie *$self, $class, $self;
     %args = (
@@ -18,7 +24,8 @@ sub new {
         close_after_write => 1,
         iomode            => '>>:utf8',
         rotationtime      => 1,
-        %args
+        callback          => $callback,
+        %args,
     );
     for my $k (keys %args) {
         *$self->{$k} = $args{$k};
@@ -38,11 +45,20 @@ sub PRINT     { shift->print(@_) }
 
 sub _gen_filename {
     my $self = shift;
-    my $time = time();
-    if ( $time > 1 ) {
-        $time = $time - $time % *$self->{rotationtime};
-    }
-    return POSIX::strftime(*$self->{pattern}, localtime($time));
+    return *$self->{callback}->(*$self);
+}
+
+sub _make_callback_from_pattern {
+    my ($pattern) = shift;
+
+    return sub {
+        my $self = shift;
+        my $time = time();
+        if ( $time > 1 ) {
+            $time = $time - $time % $self->{rotationtime};
+        }
+        return POSIX::strftime($pattern, localtime($time));
+    };
 }
 
 sub print {
@@ -101,7 +117,9 @@ File::Stamped - time stamped log file
 
 =head1 DESCRIPTION
 
-File::Stamped is utility for time stamped log file.
+File::Stamped is utility library for logging. File::Stamped object mimic file handle.
+
+You can use "myapp.log.%Y%m%d.log" style log file.
 
 =head1 METHODS
 
@@ -111,12 +129,30 @@ File::Stamped is utility for time stamped log file.
 
 This method creates new instance of File::Stamped. The arguments are followings.
 
+You need to specify one of B<pattern> or B<callback>.
 
 =over 4
 
 =item pattern : Str
 
 This is file name pattern. It is the pattern for filename. The format is POSIX::strftime(), see also L<POSIX>.
+
+=item callback : CodeRef
+
+You can use a CodeRef to generate file name.
+
+File::Stamped pass only one arguments to callback function.
+
+Here is a example code:
+
+    my $pattern = '/path/to/myapp.log.%Y%m%d.log';
+    my $f = File::Stamped->new(callback => sub {
+        my $file_stamped = shift;
+        local $_ = $pattern;
+        s/!!/$$/ge;
+        $_ = POSIX::strftime($_, localtime());
+        return $_;
+    });
 
 =item close_after_write : Bool
 
